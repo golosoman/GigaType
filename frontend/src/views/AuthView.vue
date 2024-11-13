19db9cf9cd83d61b6a72d9ac87e045f5d31e37c3
golosoman <constan.lapin2015@mail.ru>
2024-11-13 23:51:46 +0400

    add register and login page

---
+++ frontend/src/views/AuthView.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { AuthForm } from '@/component/auth';
+import { BaseLogo } from '@/component/trainer';
+import ImageUrl from '@/assets/Logo.png';
+import { ref } from 'vue';
+
+const loginData = ref('');
+const passwordData = ref('');
+</script>
+
+<template>
+    <div class="container">
+        <div class="form-wrapper">
+            <BaseLogo :logoSrc="ImageUrl" customStyleForImg="width: 150px; height: 150px;"
+                customStyleForText="color: #012E4A; font-family: 'Alata'; font-size: 64px;">
+            </BaseLogo>
+            <AuthForm v-model:login="loginData" v-model:password="passwordData" class="auth-form" />
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+}
+
+.form-wrapper {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.auth-form {
+    margin-top: 30px;
+}
+</style>
---
+++ frontend/src/views/RegisterView.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { RegisterForm } from '@/component/auth';
+import { BaseLogo } from '@/component/trainer';
+import ImageUrl from '@/assets/Logo.png';
+import { ref } from 'vue';
+
+const loginData = ref('');
+const passwordData = ref('');
+const repeatPasswordData = ref('');
+</script>
+
+<template>
+    <div class="container">
+        <div class="form-wrapper">
+            <BaseLogo :logoSrc="ImageUrl" customStyleForImg="width: 150px; height: 150px;"
+                customStyleForText="color: #012E4A; font-family: 'Alata'; font-size: 64px;">
+            </BaseLogo>
+            <RegisterForm v-model:login="loginData" v-model:password="passwordData"
+                v-model:repeatPassword="repeatPasswordData" class="register-form">
+            </RegisterForm>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+}
+
+.form-wrapper {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.register-form {
+    margin-top: 30px;
+}
+</style>
