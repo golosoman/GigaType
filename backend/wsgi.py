89e047e824f71b4dd242dcad74634f6770fe8502
golosoman <constan.lapin2015@mail.ru>
2024-12-07 13:58:18 +0400

    add cors

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,6 +1,8 @@
 from app import app
 from app.routes.api_routes import api_routes
+from flask_cors import CORS, cross_origin
 
 if __name__ == "__main__":
+    cors = CORS(app, supports_credentials=True, origins=["http://localhost:5173"])
     app.register_blueprint(api_routes)
     app.run()
