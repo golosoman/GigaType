65a52cc9d57caa5e5bfaa5d728572288e113a7d6
golosoman <constan.lapin2015@mail.ru>
2024-12-08 00:48:53 +0400

    add new api keyboard_zones

--- backend/app/routes/api_routes.py
+++ backend/app/routes/api_routes.py
@@ -3,6 +3,7 @@ from app.api.user import user_api
 from app.api.difficulty import difficulty_api
 from app.api.task import task_api
 from app.api.statistic import statistic_api
+from app.api.keyboard_zones import keyboard_zone_api
 
 api_routes = Blueprint("api_routes", __name__, url_prefix="/api")
 
@@ -10,3 +11,4 @@ api_routes.register_blueprint(user_api)
 api_routes.register_blueprint(difficulty_api)
 api_routes.register_blueprint(task_api)
 api_routes.register_blueprint(statistic_api)
+api_routes.register_blueprint(keyboard_zone_api)
